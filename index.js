12277f49d146b5f412e0e5bedbfec86d44e80d9d
user.email : khanruman799@gmail.com <khanruman799@gmail.com>
2026-08-13 13:57:09 +0530

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"
+const app = express()
+import dotenv from "dotenv"
+dotenv.config()
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res)=>{
+    res.json({message: "Hello Ruman"})
+})
+app.get("/health", (req, res)=>{
+    res.json({message: "All is good"})
+})
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
+    
+})
